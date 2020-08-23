179083f73e0f37bfaf6367494ac5a5054d138c99
Matthew Howard <mattalanhoward@gmail.com>
2020-08-23 20:48:21 +0200

    small edit to cart.model

--- models/cart.model.js
+++ models/cart.model.js
@@ -13,8 +13,8 @@ const cartSchema = new Schema(
     },
     products: [
       {
-      image: String,
       name: String,
+      imageUrl: String,
       category: String,
       price: Number,
       quantity: Number
