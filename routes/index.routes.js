f17177fb2215e163fcbb5019c272da9a8afbfc9b
Sunitha Mannam <mannam.sunitha@gmail.com>
2020-08-25 14:15:09 +0200

    menu hidden

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+router.use((req, res, next) => {
+  console.log(res.session);
+  console.log("hello");
+  next();
+});
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
 
 module.exports = router;
