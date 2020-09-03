512818ebe1b3d5ad78ab2eca0fa7a45ff8fa23b5
Matthew Howard <mattalanhoward@gmail.com>
2020-09-03 23:08:22 +0200

    cart styling / responsiveness

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 /* GET home page */
-router.get("/", (req, res, next) => res.render("index"));
+router.get("/", (req, res, next) => res.render("index", { currentUser: req.session.currentUser }));
 
 /************************************************/
 //        HTTP-GET-request: /logout
