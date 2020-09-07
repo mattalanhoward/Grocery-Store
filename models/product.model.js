674cc446675f9b4c70bac15e06aa2a8bc862199e
Matthew Howard <mattalanhoward@gmail.com>
2020-09-07 15:51:33 +0200

    small changes

--- models/product.model.js
+++ models/product.model.js
@@ -26,7 +26,7 @@ const productSchema = new Schema(
 
     category: {
       type: String,
-      enum: ["fruits", "vegetables"],
+      enum: ["fruits", "vegetables", "breads", "meat"],
     },
     price: Number,
   },
