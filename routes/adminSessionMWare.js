8d4f0064c125aa7128f9534f4ff608a1b56660a0
Sunitha Mannam <mannam.sunitha@gmail.com>
2020-09-10 20:56:16 +0200

    email fixed

--- routes/adminSessionMWare.js
+++ routes/adminSessionMWare.js
@@ -5,10 +5,13 @@ const sessionAdmin = (req, res, next) => {
   // console.log("inside sessionADmin:", req.session.currentUser);
 
   if (!req.session.currentUser) {
+    return res.redirect("/");
+  } else {
     if (!req.session.currentUser.isAdmin) {
       return res.redirect("/");
+    } else {
     }
-  } else {
+    console.log(" Hai i am not authorised ");
     // const newDate = new Date();
     // console.log(
     //   " ISIDE MIDDLEWARE ....",
